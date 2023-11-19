a397d03c0a5f663be637592c7dad83fe2c37c752
Patnaree Johnson <j.patnaree.t@gmail.com>
2023-11-18 23:09:51 -0500

    lab

--- lib/employee.py
+++ lib/employee.py
@@ -1,2 +1,51 @@
+
+
 class Employee:
-    pass
+    
+    all = []
+
+    def __init__(self,name ,salary, manager):
+        self.name = name
+        self.salary = salary
+        self.manager = manager
+        Employee.all.append(self)
+    
+    @property
+    def name(self):
+         return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str):
+            self._name = new_name
+        else:
+            raise Exception('name must be a string')
+
+    @property
+    def salary(self):
+         return self._salary
+    
+    @salary.setter
+    def salary(self, new_salary):
+        if isinstance(new_salary, float):
+            self._salary = new_salary
+        else:
+            raise Exception('salary must be a float number')
+    
+    def manager_name(self):
+        for e in Employee.all:
+            return e.manager.name
+
+    @classmethod
+    def paid_over(self, salary):
+        return [e for e in Employee.all if e.salary > salary]
+    
+    @classmethod
+    def find_by_department(self, department):
+        for e in Employee.all: 
+            if e.manager.department == department:
+                return e[0]
+
+    def tax_bracket(self):
+        return [e for e in Employee.all if e.name != self and ((self.salary - 1000) <= e.salary <= (self.salary + 1000))] 
+    
--- lib/manager.py
+++ lib/manager.py
@@ -1,2 +1,67 @@
+from employee import Employee
+from statistics import mean
+
 class Manager:
-    pass
+    
+    all = []
+
+    def __init__(self, name, department,age):
+        self.name = name
+        self.department = department
+        self.age = age
+        Manager.all.append(self)
+
+    @property
+    def name(self):
+         return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if isinstance(new_name, str):
+            self._name = new_name
+        else:
+            raise Exception('name must be a string')
+    
+    @property
+    def department(self):
+         return self._department
+    
+    @department.setter
+    def department(self, new_department):
+        if isinstance(new_department, str):
+            self._department = new_department
+        else:
+            raise Exception('name must be a string')
+        
+    @property
+    def age(self):
+         return self._age
+    
+    @age.setter
+    def age(self, new_age):
+        if isinstance(new_age, int):
+            self._age = new_age
+        else:
+            raise Exception('age must be an integer')
+
+    def employess(self):
+        return [e for e in Employee.all if e.manager == self]
+    
+    def hire_employee(self,employees_name, salary):
+            if isinstance(employees_name, Employee.all.name) and isinstance(salary, int):
+                Employee(self, employees_name)
+            else:
+                raise Exception('Cannot hire this employee, invalid employee or salary')
+            
+    @classmethod
+    def all_departments(cls):
+        return [m.department for m in Manager.all]
+    
+    @classmethod
+    def average_age(cls):
+        return round(mean([m.age for m in Manager.all]),2)
+
+
+            
+            
+
